fe541a7703270750a585981e2d5883fe2e70cae2
Thadeu Penna <thadeu.penna@id.uff.br>
2017-04-21 19:45:15 -0300

    Volta para a versão da aula

--- mola.py
+++ mola.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 
 k=1
 m=1
-g=9.8/9
+g=9.8
 dt=0.01
 x=[1]
 v=[0]
@@ -18,9 +18,9 @@ N=m*g
 for tt in range(10000): 
 	t.append(t[-1]+dt)
 	x.append(x[-1]+v[-1]*dt+0.5*a[-1]*dt**2)
-	a.append(-k*x[-1]-mu*N*np.sign(v[-1]))
+	a.append(-k*x[-1])
 	v.append(v[-1]+0.5*(a[-1]+a[-2])*dt)
 plt.figure(1)
-plt.plot(x,v)
+plt.plot(t,x)
 #plt.axes().set_aspect('equal')
 plt.show()
