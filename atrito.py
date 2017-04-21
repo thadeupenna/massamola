79628c52319fe3d5ac639622552ab3fa758c5a58
Thadeu Penna <thadeu.penna@id.uff.br>
2017-04-21 15:15:17 -0300

    Adicionada a força de atrito

--- atrito.py
+++ atrito.py
@@ -13,10 +13,12 @@ x=[1]
 v=[0]
 a=[-k*x[0]]
 t=[0]
+mu=0.003
+N=m*g
 for tt in range(10000): 
 	t.append(t[-1]+dt)
 	x.append(x[-1]+v[-1]*dt+0.5*a[-1]*dt**2)
-	a.append(-k*x[-1])
+	a.append(-k*x[-1]-mu*N*np.sign(v[-1]))
 	v.append(v[-1]+0.5*(a[-1]+a[-2])*dt)
 plt.figure(1)
 plt.plot(t,x)
